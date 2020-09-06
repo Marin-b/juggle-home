d439860b50936b453491c2e4ad3167f1dfdcce4a
Marin <Marin.bibot@hotmail.be>
2020-09-06 19:53:08 +0200

    remove log of env variable

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ const language = navigator.language.split(/[-_]/)[0];
 const  App = () => {
   const [lang, setLang] = useState('en')
   const [appUrl, setAppUrl] = useState('#')
-  console.log(process.env.REACT_APP_FIREBASE_AUTH)
+
   const fetchUrl = () => {
-    if (appUrl != '') { return }
+    if (appUrl != '#') { return }
 
     fetch('https://juggletechnologies-78310.firebaseio.com/app-url.json?auth=' + process.env.REACT_APP_FIREBASE_AUTH)
       .then(res => res.json())
